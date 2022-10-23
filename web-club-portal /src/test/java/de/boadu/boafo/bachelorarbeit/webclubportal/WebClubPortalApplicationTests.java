421ed00112c6067823baa5bb80a7b731416ad3cb
patty32 <patrick.boadu.boafo@hotmail.de>
2022-10-23 20:03:56 +0200

    initialisere base projekt

---
+++ web-club-portal /src/test/java/de/boadu/boafo/bachelorarbeit/webclubportal/WebClubPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package de.boadu.boafo.bachelorarbeit.webclubportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClubPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
