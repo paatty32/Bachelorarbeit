7d17da0b32649071da494e0cd5ac0a5c16aee503
patty32 <patrick.boadu.boafo@hotmail.de>
2022-10-24 20:46:19 +0200

    WIP - Testen, ob das Speichern von Usern und Tagebüchern klappt in der Datenbank (es klappt)

--- web-club-portal /web-club-portal-ui/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/WebClubPortalApplication.java
+++ web-club-portal /web-club-portal-ui/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/WebClubPortalApplication.java
@@ -1,8 +1,17 @@
 package de.boadu.boafo.bachelorarbeit.web.club.portal;
 
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary.Diary;
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.roles.AppUserRole;
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.user.Person;
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.user.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class WebClubPortalApplication {
@@ -11,4 +20,25 @@ public class WebClubPortalApplication {
 		SpringApplication.run(WebClubPortalApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(PersonRepository personRepository){
+
+		return args -> {
+
+			Diary diary = new Diary(2L, AppUserRole.ROLE_ATHLETE);
+			Map<AppUserRole, Diary> diaryMap = new HashMap<>();
+
+			diaryMap.put(AppUserRole.ROLE_ATHLETE, diary);
+
+			Person pat = new Person(1L,"Patrick", "Boadu Boafo", AppUserRole.ROLE_ATHLETE, diaryMap);
+
+			personRepository.save(pat);
+
+			System.out.println(personRepository.findPersonByName("Patrick").getName());
+			System.out.println(personRepository.findPersonByName("Patrick").getDiary().get(AppUserRole.ROLE_ATHLETE).getRole().toString());
+		};
+	}
+
 }
+
+
