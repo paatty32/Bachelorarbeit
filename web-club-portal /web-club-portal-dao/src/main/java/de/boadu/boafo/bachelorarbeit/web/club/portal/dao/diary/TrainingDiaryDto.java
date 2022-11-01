563a96b8d98e51f298da90e4149a89a9f506eeaa
patty32 <patrick.boadu.boafo@hotmail.de>
2022-11-01 23:58:59 +0100

    DAO WIP ADD - add new Entity TrainingDiary

---
+++ web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/diary/TrainingDiaryDto.java
@@ -0,0 +1,32 @@
+package de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary;
+
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.trainingplan.TrainingPlan;
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.user.Person;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class TrainingDiaryDto extends Diary implements TrainingDiary{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String disciplin;
+
+    @ManyToOne
+    private Person trainer;
+
+    @OneToMany
+    private List<TrainingDiaryEntryDto> entry;
+
+    @ManyToMany
+    private List<TrainingPlan> trainingPlan;
+}
