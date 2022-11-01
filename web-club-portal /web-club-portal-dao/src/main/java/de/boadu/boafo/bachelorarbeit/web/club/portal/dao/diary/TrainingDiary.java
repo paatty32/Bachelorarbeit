c4cca89e5d81ed5d7d3b39b1c56e8ad157e9e35f
patty32 <patrick.boadu.boafo@hotmail.de>
2022-11-01 23:55:06 +0100

    DAO WIP IMPLEMENT - declared getter method for entity TrainingDiaryDto

--- web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/diary/TrainingDiary.java
+++ web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/diary/TrainingDiary.java
@@ -1,4 +1,15 @@
 package de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary;
 
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.trainingplan.TrainingPlan;
+
+import java.util.List;
+
 public interface TrainingDiary {
+
+    public String getDisciplin();
+    public String getTrainer();
+    public List<TrainingDiaryEntryDto> getEntry();
+    public List<TrainingPlan> getTrainingPlan();
+
+
 }
