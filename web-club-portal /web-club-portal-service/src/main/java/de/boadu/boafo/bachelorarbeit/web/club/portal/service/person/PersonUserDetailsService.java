07e22c065f8fba1dca960e916942fbe4231e7406
patty32 <patrick.boadu.boafo@hotmail.de>
2022-11-13 23:03:00 +0100

    SERVICE REFACTORING - renanimg method

--- web-club-portal /web-club-portal-service/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/service/person/PersonUserDetailsService.java
+++ web-club-portal /web-club-portal-service/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/service/person/PersonUserDetailsService.java
@@ -3,6 +3,7 @@ package de.boadu.boafo.bachelorarbeit.web.club.portal.service.person;
 import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.person.repository.PersonRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,15 +11,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Getter
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonUserDetailsService implements UserDetailsService {
 
     private final PersonRepository personRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        return this.getPersonRepository().findPersonByName(username);
-
+        return this.getPersonRepository().findPersonBySurname(username);
     }
 }
