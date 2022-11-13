9c5c5874d885bfc4c276e3ca83f8c5154d834034
patty32 <patrick.boadu.boafo@hotmail.de>
2022-11-13 23:02:16 +0100

    SERVICE REFACTORING - renanimg method

--- web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/person/repository/PersonRepository.java
+++ web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/person/repository/PersonRepository.java
@@ -1,13 +1,13 @@
 package de.boadu.boafo.bachelorarbeit.web.club.portal.dao.person.repository;
 
 import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.person.Person;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 
     Person save(Person person);
 
-    Person findPersonByName(String name);
+    Person findPersonBySurname(String name);
 }
