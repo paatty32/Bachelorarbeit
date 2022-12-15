f736802f5f3f21917bbdb3db11061c0b4117a435
patty32 <patrick.boadu.boafo@hotmail.de>
2022-12-15 14:07:42 +0100

    UI ADD - implement interface for applicationscope service to collect all ui services under one interface

---
+++ web-club-portal /web-club-portal-ui/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/service/ApplicationScopeService.java
@@ -0,0 +1,5 @@
+package de.boadu.boafo.bachelorarbeit.web.club.portal.service;
+
+public interface ApplicationScopeService extends CompetitionDiaryUiService, RegistrationUiService,
+        TrainingsDiaryUiService{
+}
