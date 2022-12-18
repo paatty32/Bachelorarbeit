c53a9239d65a4402166afa29bc312f35ca2ed7b2
patty32 <patrick.boadu.boafo@hotmail.de>
2022-12-18 15:48:21 +0100

    DAO REFACTORING - Changing Collection typ from List to set

--- web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/person/MutablePerson.java
+++ web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/person/MutablePerson.java
@@ -3,12 +3,13 @@ package de.boadu.boafo.bachelorarbeit.web.club.portal.dao.person;
 import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.roles.AppUserRole;
 
 import java.util.List;
+import java.util.Set;
 
 public interface MutablePerson {
 
     void setName(String name);
     void setSurname(String name);
     void setPassword(String password);
-    void setRoles(List<AppUserRole> roles);
+    void setRoles(Set<AppUserRole> roles);
 
 }
