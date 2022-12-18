6ac471ed42374062cf15d12ed9af44277a2780c8
patty32 <patrick.boadu.boafo@hotmail.de>
2022-12-18 17:58:49 +0100

    UI ADD - method to get user roles

--- web-club-portal /web-club-portal-ui/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/config/security/SecurityService.java
+++ web-club-portal /web-club-portal-ui/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/config/security/SecurityService.java
@@ -2,11 +2,16 @@ package de.boadu.boafo.bachelorarbeit.web.club.portal.config.security;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.server.VaadinServletRequest;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Component
 public class SecurityService {
@@ -21,6 +26,40 @@ public class SecurityService {
         }
         // Anonymous or no authentication.
         return null;
+    }
+
+    private Collection<? extends GrantedAuthority> getUserAuthority(){
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        Object principal = context.getAuthentication().getPrincipal();
+
+        if( principal instanceof UserDetails){
+
+            Collection<? extends GrantedAuthority> authorities = ((UserDetails) principal).getAuthorities();
+
+            return authorities;
+
+        }
+
+        return null;
+
+    }
+
+    public List<String> getUserRoles(){
+
+        List<String> userRoles = new ArrayList<>();
+
+        Collection<? extends GrantedAuthority> userAuthority = this.getUserAuthority();
+
+        if(userAuthority != null) {
+            for (GrantedAuthority authority : userAuthority) {
+
+                userRoles.add(authority.toString());
+
+            }
+        }
+
+        return userRoles;
     }
 
     public void logout() {
