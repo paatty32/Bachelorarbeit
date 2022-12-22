c07ee1beb331bf99f28efcedbe7d382a98360835
patty32 <patrick.boadu.boafo@hotmail.de>
2022-12-22 15:32:06 +0100

    DAO REFACOTIRNG - renaming training diary

--- web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/diary/training/repository/TrainingsDiaryEntryRepository.java
+++ web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/diary/training/repository/TrainingsDiaryEntryRepository.java
@@ -1,14 +1,14 @@
 package de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary.training.repository;
 
-import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary.training.TrainingDiaryEntryDto;
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary.training.TrainingDiaryEntryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TrainingsDiaryEntryRepository extends JpaRepository<TrainingDiaryEntryDto, Long> {
+public interface TrainingsDiaryEntryRepository extends JpaRepository<TrainingDiaryEntryDTO, Long> {
 
-    TrainingDiaryEntryDto save(TrainingDiaryEntryDto diaryEntryDto);
+    TrainingDiaryEntryDTO save(TrainingDiaryEntryDTO diaryEntryDto);
 
-    TrainingDiaryEntryDto findEntryById(Long id);
+    TrainingDiaryEntryDTO findEntryById(Long id);
 
 }
