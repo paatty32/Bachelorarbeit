7cd4b7e7ef4c1cd42e8d1a99df47a13c59d3b765
patty32 <patrick.boadu.boafo@hotmail.de>
2022-12-22 15:20:32 +0100

    UI ADD - service for trainingplan

---
+++ web-club-portal /web-club-portal-service/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/service/diary/trainingplanentry/TrainingPlanEntryService.java
@@ -0,0 +1,9 @@
+package de.boadu.boafo.bachelorarbeit.web.club.portal.service.diary.trainingplanentry;
+
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary.trainingplan.MutableTrainingPlanEntry;
+
+public interface TrainingPlanEntryService {
+
+    void updateEntry(MutableTrainingPlanEntry entry);
+
+}
