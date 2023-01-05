ea370ef7b34721ac128b016da98dac77d5fdd90e
patty32 <patrick.boadu.boafo@hotmail.de>
2023-01-06 00:18:33 +0100

    DAO REFACTORING - deleting unused lines

--- web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/diary/Diary.java
+++ web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/diary/Diary.java
@@ -1,10 +1,6 @@
 package de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary;
 
-import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.roles.AppUserRole;
-import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.roles.DiaryType;
-import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.trainingsgroup.TrainingsGroup;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
