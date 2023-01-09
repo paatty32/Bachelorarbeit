624a76c73d181f54e15084d4922b413630b5e2a4
patty32 <patrick.boadu.boafo@hotmail.de>
2023-01-09 18:36:03 +0100

    ADD methods for trainingplan component to facade and application sciope

--- web-club-portal /web-club-portal-ui/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/service/ApplicationScopeService.java
+++ web-club-portal /web-club-portal-ui/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/service/ApplicationScopeService.java
@@ -1,5 +1,5 @@
 package de.boadu.boafo.bachelorarbeit.web.club.portal.service;
 
 public interface ApplicationScopeService extends CompetitionDiaryUiService, RegistrationUiService,
-        TrainingsDiaryUiService, TrainingPlanUiService, GroupUiService, PersonUiService{
+        TrainingsDiaryUiService, TrainingPlanUiService, GroupUiService, PersonUiService, TrainingPlanEntryUiService{
 }
