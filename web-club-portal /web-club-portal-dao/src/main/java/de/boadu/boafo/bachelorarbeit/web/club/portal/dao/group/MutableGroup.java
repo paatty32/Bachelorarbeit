a7ab846163c0f7d5703c2fe1246bab5484971fe5
patty32 <patrick.boadu.boafo@hotmail.de>
2023-01-09 18:42:43 +0100

    DAO - add method

--- web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/group/MutableGroup.java
+++ web-club-portal /web-club-portal-dao/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/dao/group/MutableGroup.java
@@ -1,5 +1,9 @@
 package de.boadu.boafo.bachelorarbeit.web.club.portal.dao.group;
 
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary.trainingplan.TrainingPlanEntryDTO;
+
+import java.util.Set;
+
 public interface MutableGroup {
 
     void setId(Long id);
@@ -11,5 +15,7 @@ public interface MutableGroup {
     void setDescription(String description);
 
     void setAdminId(Long id);
+
+    void setTrainingPlanEntry(Set<TrainingPlanEntryDTO> entries);
 
 }
