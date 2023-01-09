97c8a4b670cc448d059548abc38e4e216f17a370
patty32 <patrick.boadu.boafo@hotmail.de>
2023-01-09 18:39:21 +0100

    SERVICE ADD - add method to trainingplanentry

--- web-club-portal /web-club-portal-service/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/service/diary/trainingplanentry/TrainingPlanEntryServiceImpl.java
+++ web-club-portal /web-club-portal-service/src/main/java/de/boadu/boafo/bachelorarbeit/web/club/portal/service/diary/trainingplanentry/TrainingPlanEntryServiceImpl.java
@@ -1,6 +1,7 @@
 package de.boadu.boafo.bachelorarbeit.web.club.portal.service.diary.trainingplanentry;
 
 import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary.trainingplan.MutableTrainingPlanEntry;
+import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary.trainingplan.TrainingPlanEntry;
 import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary.trainingplan.TrainingPlanEntryDTO;
 import de.boadu.boafo.bachelorarbeit.web.club.portal.dao.diary.trainingplan.repository.TrainingPlanEntryRepository;
 import lombok.AccessLevel;
@@ -21,5 +22,10 @@ public class TrainingPlanEntryServiceImpl implements TrainingPlanEntryService{
 
         this.getTrainingPlanEntryRepository().save((TrainingPlanEntryDTO) entry);
 
+    }
+
+    @Override
+    public TrainingPlanEntry getTrainingPlanEntry(Long id) {
+        return this.getTrainingPlanEntryRepository().findEntryById(id);
     }
 }
